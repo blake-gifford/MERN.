0428531742b36aad62c546caf5dc04c1a90dda11
blake-gifford <blakegifford01@gmail.com>
2021-05-04 20:36:08 -0700

    assignments

---
+++ assignments/ninja.js
@@ -0,0 +1,51 @@
+
+class Ninja {
+    constructor(name, health, speed, strength){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log(this.name);
+    }
+    showStats(){
+        console.log("Ninja Name: " + this.name)
+        console.log("Ninja Health: " + this.health)
+        console.log("Ninja Speed: " + this.speed)
+        console.log("Ninja Strength: " + this.strength)
+    }
+    drinkSake(){
+        this.health += 10
+        console.log(this.health)
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name, health, speed, strength, wisdom){
+        super(name, 200, 10, 10, 10);
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        console.log('What one programmer can do in one month, two programmers can do in two months.');
+        console.log(this.name);
+    }
+    drinkSake(){
+        console.log("Ninja Name: " + this.name)
+        console.log("Ninja Health: " + this.health)
+        console.log("Ninja Speed: " + this.speed)
+        console.log("Ninja Strength: " + this.strength)
+    }
+}
+
+let blake = new Ninja('Blake', 100);
+let senseiBlake = new Sensei('Blake', )
+console.log(blake);
+
+blake.sayName();
+
+blake.drinkSake();
+
+senseiBlake.speakWisdom();
+
+senseiBlake.drinkSake();
