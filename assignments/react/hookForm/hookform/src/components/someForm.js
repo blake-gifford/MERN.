9f7a6d1bed8c3e5f3d2ad07bf015e317dc4403d9
blake-gifford <blakegifford01@gmail.com>
2021-05-09 23:22:28 -0700

    daily assignment commit

--- assignments/react/hookForm/hookform/src/components/someForm.js
+++ assignments/react/hookForm/hookform/src/components/someForm.js
@@ -3,14 +3,38 @@ import React, { useState } from 'react'
 const SomeForm = () => {
 
     const [firstName, setFirstName] = useState("");
+    const [firstNameError, setFirstNameError] = useState[""];
     const [lastName, setLastName] = useState("");
+    const [lastNameError, setLastNameError] = useState[""];
     const [email, setEmail] = useState("");
-    
+    const [emailError, setEmailError] = useState[""];
     const [formSubmitted, setFormSubmitted] = useState(false);
+    
 
     const submitHandler = e => {
         e.preventDefault();
         setFormSubmitted(true);
+    }
+
+    const handleFirstName = e => {
+        setFirstNameError(e.target.value);
+        if(e.target.value.length < 1){
+            setFirstNameError("First Name Required");
+        }
+    }
+
+    const handleLastName = e => {
+        setLastNameError(e.target.value);
+        if(e.target.value.length < 1){
+            setLastNameError("Last Name Required");
+        }
+    }
+
+    const handleEmail = e => {
+        setEmailError(e.target.value);
+        if(e.target.value.length < 1){
+            setEmailError("Email Required");
+        }
     }
 
     return (
@@ -24,14 +48,32 @@ const SomeForm = () => {
                     <div>
                         <label htmlFor="firstName">First Name: </label>
                         <input type="text" name="firstName" onChange={ e => setFirstName(e.target.value) } />
+                        {
+                            firstNameError ?
+                            <p> { handleFirstName }</p> 
+                            :
+                            ''
+                        }
                     </div>
                     <div>
                         <label htmlFor="lastName">Last Name: </label>
                         <input type="text" name="lastName" onChange={ e => setLastName(e.target.value) } />
+                        {
+                            lastNameError ?
+                            <p> { handleLastName }</p> 
+                            :
+                            ''
+                        }
                     </div>
                     <div>
                         <label htmlFor="email">Email: </label>
                         <input type="text" name="email" onChange={ e => setEmail(e.target.value) } />
+                        {
+                            emailError ?
+                            <p> { handleEmail }</p> 
+                            :
+                            ''
+                        }
                     </div>
                     <div>
                         <label htmlFor="password">Password: </label>
